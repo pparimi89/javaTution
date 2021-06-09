de4344dfeff6cda62eb65a59ddca5201cb6fd765
pradeepq <parimi1230@gmail.com>
2021-06-09 12:31:49 +0530

    session 2

---
+++ notes/HelloWorld.java
@@ -0,0 +1,13 @@
+
+//Sample program hello world program
+import java.io.*;
+
+public class HelloWorld{
+
+  public static void main(String args[]){
+
+   System.out.println("*** Hello World *** ");
+    
+  }
+
+}
