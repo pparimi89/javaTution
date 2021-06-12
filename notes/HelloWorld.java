a998b33a6bfd87a57ad151eea89c4564fbafc78d
pradeepq <parimi1230@gmail.com>
2021-06-12 17:54:44 +0530

    session 3

--- notes/HelloWorld.java
+++ notes/HelloWorld.java
@@ -4,10 +4,19 @@ import java.io.*;
 
 public class HelloWorld{
 
-  public static void main(String args[]){
+  public static void main(String args[]) throws InterruptedException{
 
    System.out.println("*** Hello World *** ");
-    
+   System.out.println("*** Hello World *** ");
+   System.out.println("*** Hello World *** ");
+   System.out.println("*** Hello World 1*** ");
+   System.out.println("*** Hello World 2*** ");
+   System.out.println("*** Hello World 3*** ");
+   System.out.println("*** Hello World 4*** ");
+
+   Thread.sleep(100000);
+
+
   }
 
 }
