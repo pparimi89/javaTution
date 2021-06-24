0fbfbb46b081c454861d069554a4b50513009036
pradeepq <parimi1230@gmail.com>
2021-06-24 09:42:52 +0530

    session 7 packages

---
+++ src/com/vitsolutions/javatutions/cse/Department.java
@@ -0,0 +1,9 @@
+package com.vitsolutions.javatutions.cse;
+
+import com.vitsolutions.javatutions.examcell.ExamLocation;
+public class Department {
+    public static void main(String[] args) {
+        ExamLocation examLocation = new ExamLocation();
+        examLocation.printExamCenter();
+    }
+}
